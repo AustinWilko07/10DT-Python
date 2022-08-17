15462b34559bc6ca009fb0efc0e986359199329a
AustinWilko07 <103608436+AustinWilko07@users.noreply.github.com>
2022-08-18 10:00:59 +1200

    Add files via upload

---
+++ Lesson1/Python1.py
@@ -0,0 +1,17 @@
+  # A Wilkinson
+
+  # Variables
+
+  # declaration of variables
+  name = "Lion"
+  numlegs = 4
+  height = 2.5
+  hastail = True
+
+  # Output to screen
+  print(name)
+  print(numlegs)
+  print(height)
+  print(hastail)
+
+  
